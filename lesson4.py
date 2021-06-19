bd1a95937a2ab400ad4d19fb57a3957f1d8644e7
monster <alishirinova@gmail.com>
2021-06-19 12:39:10 +0300

    lesson4.py

--- lesson4.py
+++ lesson4.py
@@ -1,1 +1,16 @@
-print(123)
+value=input('введи целое число ')
+# if value.isdigit():
+#     value=int(value)
+#     result=value*2
+#     print(result)
+# else:
+#     print('вы ввели не число')
+
+try:
+    value=float(value)
+    result=value*2
+    print(result)
+except:
+    print('число не может быть приведено к типу float')
+    result=0
+    print(result)
