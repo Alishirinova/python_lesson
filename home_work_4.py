181b5148a3dc1d08160a3e9a7ee3933104a6ce84
monster <alishirinova@gmail.com>
2021-06-23 00:03:22 +0300

    home_work_4.py

--- home_work_4.py
+++ home_work_4.py
@@ -1,5 +1,13 @@
-my_list= [1, 2, 3, 50, 120, 150, 1100]
+my_list= [500, 1, 2, 3, 50, 120, 150, 1100]
 for n in my_list:
     if n>100:
         print(n)
     else: None
+
+###################################################2)
+my_result=[]
+for n in my_list:
+    if n>100:
+        my_result.append(n)
+        print(list(my_result))
+
