e9396c3c8d46b182021d7a6426bf5c45e691f438
monster <alishirinova@gmail.com>
2021-06-22 21:53:54 +0300

    home_work3.py

---
+++ home_work3.py
@@ -0,0 +1,21 @@
+value=750
+new_value = value / 2 if value < 100 else value *-1
+print(new_value)
+
+#####################################################
+value=70
+new_value = 1 if value < 100 else 0
+print(new_value)
+
+#####################################################
+value=800
+new_value = True if value < 100 else False
+print(new_value)
+
+#####################################################
+# my_str='HGFtgvjsdh'
+# print(type(my_str))
+
+
+
+
