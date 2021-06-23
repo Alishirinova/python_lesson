311df78baa5e35f6c2270428eb3c4203ebb4aa31
monster <alishirinova@gmail.com>
2021-06-23 19:12:55 +0300

    home_work_4.py

--- lesson4_3.py
+++ lesson4_3.py
@@ -10,27 +10,27 @@ for symbol in test_str:
     join_str ="_".join(result)
     print(join_str)
 
-#tuple - кортеж, неизменяемый тип
-#list - список, изменяемый тип
-my_tuple = (1,2,"qwe", (-1,0),None)
-print(type(my_tuple),my_tuple)
+# #tuple - кортеж, неизменяемый тип
+# #list - список, изменяемый тип
+# my_tuple = (1,2,"qwe", (-1,0),None)
+# print(type(my_tuple),my_tuple)
 
-my_list = [1,2,"qwe", (-1,0),None]#[1,2,3,3.0]
-print(type(my_list),my_list)
-index=-1 #тут задаем
-#my_tuple=my_tuple[::-1]
-my_list[index] = 3
-value_tuple = my_tuple[index]
-value_list = my_list[index]
-print(value_tuple,value_list)
+# my_list = [1,2,"qwe", (-1,0),None]#[1,2,3,3.0]
+# print(type(my_list),my_list)
+# # index=-1 #тут задаем
+# # #my_tuple=my_tuple[::-1]
+# # my_list[index] = 3
+# # value_tuple = my_tuple[index]
+# # value_list = my_list[index]
+# # print(value_tuple,value_list)
 
 #СРЕЗЫ как в строках
 
-#приведение к типам
-new_list=list(my_tuple)
-new_tuple=tuple(my_list)
-print(type(new_tuple),new_tuple)
-print(type(new_list),new_list)#можем поменять кортеж и лист
+# #приведение к типам
+# new_list=list(my_tuple)
+# new_tuple=tuple(my_list)
+# print(type(new_tuple),new_tuple)
+# print(type(new_list),new_list)#можем поменять кортеж и лист
 
 
 
