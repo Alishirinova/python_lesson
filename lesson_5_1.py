a993442ff7ac6b40cdef7db9c6cc2ae798b045e3
monster <alishirinova@gmail.com>
2021-06-23 20:52:08 +0300

    lesson_5_1.py

--- lesson_5_1.py
+++ lesson_5_1.py
@@ -17,13 +17,51 @@ for _ in range(my_symbol_count): # _ имя переменной, которую
     print(res_msg.strip()) # стрип - избавляет от лишней табуляции
 
 ##################3
-my_str = 'bla BLA car'
+my_str = "bla BLA car"
+print(len(my_str))
 my_str = my_str.lower()
 symbols_heap = []
+
 for symbol in my_str:
     if symbol not in symbols_heap:
         symbols_heap.append(symbol)
-    res_len = len(symbols_heap)
-    print(res_len) ## тут чет не то, нужно посмотреть видео и доработать
+res_len = len(symbols_heap)
+print(res_len) #задача была вывести кол-во символов в тексте
 
+##################3
+my_str = 'qwerty'
+my_list = []
+for index in range (len(my_str)):
+    if not index % 2:
+        symbol = my_str[index]
+        my_list.append(symbol)
+# for index, symbol in enumerate(my_str):
+#     if not index % 2:
+#         my_list.append(symbol) альтернативное решение с помощью enumerate
+print(my_list) #задача была заполнить список символами которые стоят на четных местах
+
+##################4
+my_str='qwerty'
+my_list=[]
+str_index = [3, 2, 5, 1, 0, 5]
+for index in str_index:
+    symbol = my_str[index]
+    my_list.append(symbol)
+print(my_list)#заполнили список символами из переменной, которые стоят на местах с задаными индексами
+
+##################5
+my_number = 2315132131531351315351
+digit_count = len(str(my_number))
+print(digit_count) # подсчитали кол-во символов в переменной
+
+##################6
+# в пайтоне существует алфавит символов, втч цифр. ASCii
+#ord("@")  - узнать какой номер у символа
+#chr(100)  - проверить, какой символ стоит под этим номером
+
+number_str = str (my_number)
+max_symbol = max(number_str) #не работает для разных типов данных
+print(max_symbol) #нашли наибольшее число в переменной
+
+##################7
 
