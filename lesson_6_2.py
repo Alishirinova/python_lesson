db3e556c988c48692528b29e1c4e069e86872c0d
monster <alishirinova@gmail.com>
2021-06-30 20:48:45 +0300

    lesson_6_2.py

---
+++ lesson_6_2.py
@@ -0,0 +1,33 @@
+#множество set, уникальные элементы
+my_list = [1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 2]
+my_set=set(my_list)
+print(my_set)
+# my_list_unique = list(set(my_list)) - убивает дубли , сет не сохраняет порядок
+my_list_unique = list(my_set)
+print(my_list_unique)
+new_set = {1,2,54,53,54,54}
+print(new_set)
+new_set=list(new_set)
+print(new_set)
+
+#см лессон 5, номр 3 сколько разных символов встречается в строке:
+my_str = 'qqqqqqqwwwwwwwwweeeeeeeeeerrrrrrrtttttty'
+res_len =len(set(my_str))
+print(res_len)
+# как подсчитать кол-во записей каждого символа
+for symbol in set(my_str):
+    print(symbol, my_str.count(symbol))
+
+######работа с множествами ( инер джоин, лефт и тд)
+my_str_1 ='qwerty'
+my_str_2 = 'qweasd'
+my_set_1 = set(my_str_1)
+my_set_2 = set(my_str_2)
+intersection = my_set_1.intersection(my_set_2) #общие элементы
+print(intersection)
+union = my_set_1.union(my_set_2) #все элементы двух множест
+print(union)
+difference =my_set_1.difference(my_set_2) #уникальные, неповторяющиеся элементы из my_set_1
+print(difference)
+difference_2 = my_set_2.difference(my_set_1) #уникальные, неповторяющиеся элементы из my_set_2
+print(difference_2)
