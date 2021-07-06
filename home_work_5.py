3a3a044e33829c3e40c3524205f732ed9c88cb29
monster <alishirinova@gmail.com>
2021-07-06 22:23:33 +0300

    home_work_5.py

--- home_work_5.py
+++ home_work_5.py
@@ -6,7 +6,6 @@ print(value.count('0'))
 # 2. Дано целое число (int).
 # Определить сколько нулей в конце этого числа.
 # Например для числа 1002000 - три нуля
-
 my_value = '100200000'
 my_list = list(my_value)
 my_list.reverse()
@@ -15,3 +14,23 @@ my_value_new = int(my_value_new)
 my_value_new = str(my_value_new)
 result = len(my_value) - len(my_value_new)
 print(result)
+
+# 3. Даны списки my_list_1 и my_list_2.
+# Создать список my_result в который вначале поместить
+# элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
+
+my_list_1 = [1, 2, 3, 4, 6, 5, 7]
+my_list_2 = [10, 20, 30, 40, 50]
+my_result=[]
+for index in range (len(my_list_1)):
+    if not index % 2:
+        symbol = my_list_1[index]
+        my_result.append(symbol)
+for index in range (len(my_list_2)):
+    if index % 2:
+        symbol = my_list_2[index]
+        my_result.append(symbol)
+print(my_result)
+
+
+
